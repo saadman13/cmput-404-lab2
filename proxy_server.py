1fad7c93a90c7397e8950c87bc5c5c7c71cdf144
Saadman Islam Khan <saadmanislamkhan@Saadmans-MacBook-Pro.local>
2023-01-16 19:20:46 -0700

    update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -7,12 +7,12 @@ BYTES_TO_READ = 4096
 PROXY_SERVER_HOST = "127.0.0.1"
 PROXY_SERVER_PORT = 8080
 
-def send_request(host, port, request_data):
+def send_request(host, port, request):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         
         #sending data to host
         client_socket.connect((host, port))
-        client_socket.send(request_data)
+        client_socket.send(request)
         client_socket.shutdown(socket.SHUT_WR) # shut down socket but not entirely ( so we are not going to 
         #send anything back to google. Everything we wanted to send we already did)
 
@@ -20,7 +20,7 @@ def send_request(host, port, request_data):
       
         data = client_socket.recv(BYTES_TO_READ)
         result = b'' + data
-        while(len(data) > 0):
+        while len(data) > 0:
             data = client_socket.recv(BYTES_TO_READ)
             result += data
             
@@ -29,7 +29,7 @@ def send_request(host, port, request_data):
 def handle_connection(conn, addr):
     with conn:
         print(f"Connected by {addr}")
-        request = b""
+        request = b''
         while True:
             data = conn.recv(BYTES_TO_READ)
             if not data:
@@ -38,8 +38,9 @@ def handle_connection(conn, addr):
             print(data)
             request += data
 
-        response = send_request(PROXY_SERVER_HOST, PROXY_SERVER_PORT, data)
-        conn.sendall(reponse)
+        response = send_request("www.google.com", 80, request)
+        res =conn.sendall(response)
+        print(res)
 
 def start_server():
      with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
